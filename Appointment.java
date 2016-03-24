24052853abdb04066c63b489b35ee797106125f7
jdebr <jd.debr@gmail.com>
2016-03-24 15:03:49 -0600

    Updated comments

--- Appointment.java
+++ Appointment.java
@@ -18,52 +18,52 @@ public class Appointment {
 		this.participants = part;
 	}
 
-
+	// Returns the unique name of an appointment
 	public String getName() {
 		return name;
 	}
 
-
+	// Sets the unique name of an appointment
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
+	// Returns the date of the appointment (only a Day of the week in this implementation)
 	public DayOfWeek getDay() {
 		return day;
 	}
 
-
+	// Sets the day of a week for an appointment, type is java.time.DayOfWeek
 	public void setDay(DayOfWeek day) {
 		this.day = day;
 	}
 
-
+	// Gets the java.time.LocalTime start time of an appointment
 	public LocalTime getStart_time() {
 		return start_time;
 	}
 
-
+	// Sets the java.time.LocalTime start time of an appointment
 	public void setStart_time(LocalTime start_time) {
 		this.start_time = start_time;
 	}
 
-
+	// Gets the java.time.LocalTime end time of an appointment
 	public LocalTime getEnd_time() {
 		return end_time;
 	}
 
-
+	// Sets the java.time.LocalTime end time of an appointment
 	public void setEnd_time(LocalTime end_time) {
 		this.end_time = end_time;
 	}
 
-
+	// Gets the integer array of the appointment participants, contains node IDs
 	public int[] getParticipants() {
 		return participants;
 	}
 
-
+	// Sets the integer array of the appointment participants
 	public void setParticipants(int[] participants) {
 		this.participants = participants;
 	}
--- Event.java
+++ Event.java
@@ -11,34 +11,42 @@ public class Event {
 		this.op = op;
 	}
 
+	// Returns the appointment object associated with an event
 	public Appointment getAppointment() {
 		return appointment;
 	}
 
+	// Sets an appointment object to be associated with an event
 	public void setAppointment(Appointment appointment) {
 		this.appointment = appointment;
 	}
 
+	// Gets the operation of the event, a String that is either "Create" or "Delete"
 	public String getOp() {
 		return op;
 	}
 
+	// Sets the operation of the event, a String that is either "Create" or "Delete"
 	public void setOp(String op) {
 		this.op = op;
 	}
-
+	
+	// Gets the logical time of an event, an integer value
 	public int getTime() {
 		return time;
 	}
 
+	// Sets the logical time of an event, an integer value
 	public void setTime(int time) {
 		this.time = time;
 	}
 
+	// Gets the node ID of the creator of the event, an integer value
 	public int getNodeID() {
 		return nodeID;
 	}
 
+	// Sets the node ID of the creator of the event, an integer value
 	public void setNodeID(int nodeID) {
 		this.nodeID = nodeID;
 	}
